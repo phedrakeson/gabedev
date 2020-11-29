4e4fd4e44840193b555759f507dff4deb707bb30
phedrakeson <gabrield.developer@gmail.com>
2020-11-29 20:16:14 -0300

    carrousel project button

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,10 +1,9 @@
 document.querySelector('#items').addEventListener('wheel', event => {
   event.preventDefault();
+
   if(event.deltaY > 0) {
     event.target.scrollBy(-400, 0);
-    console.log('BAIXO')
   } else {
     event.target.scrollBy(400, 0);
-    console.log('CIMA')
   }
 })
